b34b74dbb72885e239e968779aec747dab26443a
Ricardo Pajarito <ricardopch15@gmail.com>
2019-02-16 12:46:43 -0600

    Se agrega el main
    Lo que faltaba

--- mergesort.c
+++ mergesort.c
@@ -78,3 +78,19 @@ void printArray(int A[], int size)
 		printf("%d ", A[i]); 
 	printf("\n"); 
 } 
+
+/* Driver program to test above functions */
+int main() 
+{ 
+	int arr[] = {12, 11, 13, 5, 6, 7}; 
+	int arr_size = sizeof(arr)/sizeof(arr[0]); 
+
+	printf("Given array is \n"); 
+	printArray(arr, arr_size); 
+
+	mergeSort(arr, 0, arr_size - 1); 
+
+	printf("\nSorted array is \n"); 
+	printArray(arr, arr_size); 
+	return 0; 
+} 
